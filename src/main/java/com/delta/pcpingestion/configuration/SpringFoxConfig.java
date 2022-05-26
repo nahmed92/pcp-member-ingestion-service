2d9050e42523d6a3673d00b647596eb972b85381
Surendra K Kalahasthi <skalahasthi2@delta.org>
2022-05-26 16:37:06 -0700

    testing tibco api

--- src/main/java/com/delta/pcpingestion/configuration/SpringFoxConfig.java
+++ src/main/java/com/delta/pcpingestion/configuration/SpringFoxConfig.java
@@ -2,6 +2,9 @@ package com.delta.pcpingestion.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -11,18 +14,40 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+@Configuration
+public class SpringFoxConfig implements WebMvcConfigurer {
+	
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.delta.pcpingestion.controller"))
-				
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.deltadental.pcp.calculation.controller"))
+				.paths(PathSelectors.any())
+				.build();
 	}
-	
+
+	//FIXME: revisit
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/null/api-docs", "/api-docs").setKeepQueryParams(true);
+		registry.addRedirectViewController("/null/swagger-resources/configuration/ui",
+				"/swagger-resources/configuration/ui");
+		registry.addRedirectViewController("/null/swagger-resources/configuration/security",
+				"/swagger-resources/configuration/security");
+		registry.addRedirectViewController("/null/swagger-resources", "/swagger-resources");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/api/swagger-ui.html**")
+				.addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+		registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("PCP Member Ingestion Service").description("PCP Member Ingestion Service").version("1.0").build();
+		String description = "Delta Dental Ins -  PCP Member Ingestion Service";
+		return new ApiInfoBuilder().title("Swagger API Doc").description(description).version("1.0.0").build();
 	}
 }
--- src/main/java/com/delta/pcpingestion/scheduler/PublisherScheduler.java
+++ src/main/java/com/delta/pcpingestion/scheduler/PublisherScheduler.java
@@ -16,7 +16,7 @@ public class PublisherScheduler {
 	@Autowired
 	private PublisherService publisherService;
 
-	@Scheduled(cron = "${job.publisher.cron.scheduler:*/30 * * * * *}")
+	@Scheduled(cron = "*/30 * * * * *")
 	@MethodExecutionTime
 	public void schedulePostContractDataOnPCPCalculationFixedRateTask() {
 		log.info("START PublisherScheduler.schedulePostContractDataOnPCPCalculationFixedRateTask()");
