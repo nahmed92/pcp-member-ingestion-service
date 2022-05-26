a22625a1c8b6d8e3d6017a358010e757b4db9127
Surendra K Kalahasthi <skalahasthi2@delta.org>
2022-05-26 14:32:05 -0700

    updated config

--- src/main/java/com/delta/pcpingestion/configuration/SpringFoxConfig.java
+++ src/main/java/com/delta/pcpingestion/configuration/SpringFoxConfig.java
@@ -17,7 +17,7 @@ public class SpringFoxConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.delta.pcpingestion"))
+				.apis(RequestHandlerSelectors.basePackage("com.delta.pcpingestion.controller"))
 				
 				.paths(PathSelectors.any()).build();
 	}
