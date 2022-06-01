c880556022480e9dcf193bf46430c3bd616bd456
Surendra K Kalahasthi <skalahasthi2@delta.org>
2022-06-01 10:31:39 -0700

    updated table script as per entity

--- SQL_Scripts/contract_2022_06_01.sql
+++ SQL_Scripts/contract_2022_06_01.sql
@@ -1,11 +1,13 @@
 
+DROP TABLE IF EXISTS dbo.contract ;
+
 CREATE TABLE dbo.contract (
 	id varchar(200) NOT NULL,
 	contract_json varchar(MAX) ,
 	mtv_person_ids varchar(2000) ,
 	claims_ids varchar(2000) ,
 	state_codes varchar(1000) ,
-	num_of_attempt int NULL,
+	num_of_retries int NULL,
 	num_of_enrollee int NULL,
 	publish_status varchar(10) NOT NULL,
 	created_at datetime NULL,
@@ -20,5 +22,4 @@ CREATE  INDEX [idx_v_contract_id] ON [dbo].[contract](v_contract_id);
 CREATE  INDEX [idx_publish_status] ON [dbo].[contract](publish_status);
 
 CREATE  INDEX [idx_state_codes] ON [dbo].[contract](state_codes);
-
 
