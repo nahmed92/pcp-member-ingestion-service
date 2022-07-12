580c8f819e521afe68094eb3543ae3ac6d63aae8
Surendra K Kalahasthi <skalahasthi2@delta.org>
2022-07-12 10:42:12 -0700

    fixed junits

---
+++ src/test/java/com/delta/pcpingestion/repo/IngestionControllerRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.delta.pcpingestion.repo;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.delta.pcpingestion.entity.IngestionControllerEntity;
+import com.delta.pcpingestion.enums.ControlStatus;
+
+@SpringBootTest
+public class IngestionControllerRepositoryTest {
+
+	@Autowired
+	IngestionControllerRepository repo;
+	
+	@BeforeEach
+	public void init() {
+		IngestionControllerEntity entity = IngestionControllerEntity.builder().id("123")
+				.runId("123")
+				.serviceInstanceId("localhost")
+				.status(ControlStatus.CREATED)
+				.states("CA,NY")
+				.build();
+		
+		repo.save(entity);
+	}
+	
+	@Test
+	public void testFindAll() {
+		List<IngestionControllerEntity> entities =  repo.findAll();
+		
+		assertNotNull(entities);
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/test/java/com/delta/pcpingestion/service/IngestionControllerServiceTest.java
@@ -0,0 +1,95 @@
+package com.delta.pcpingestion.service;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.delta.pcpingestion.entity.ContractEntity;
+import com.delta.pcpingestion.enums.State;
+import com.delta.pcpingestion.interservice.tibco.TibcoClient;
+import com.delta.pcpingestion.repo.ContractDAO;
+import com.delta.pcpingestion.repo.IngestionControllerRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.delta.pcpingestion.entity.ContractEntity;
+import com.delta.pcpingestion.entity.IngestionControllerEntity;
+import com.delta.pcpingestion.enums.State;
+import com.delta.pcpingestion.interservice.tibco.TibcoClient;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration
+@TestInstance(Lifecycle.PER_CLASS)
+@Configuration
+public class IngestionControllerServiceTest {
+
+	@Mock
+	private ContractDAO repository;
+
+	@Mock
+	private TibcoClient tibcoClient;
+	
+	@Mock
+	private IngestionControllerRepository repo;
+
+	@Mock
+	private com.delta.pcpingestion.interservice.PCPConfigServiceClient configClient;
+
+	@InjectMocks
+	
+	private ContractIngester contractIngester;
+	
+	private IngestionControllerService ingestionControllerService;
+	
+
+
+	@Test
+	public void testIngestByState() throws Exception {
+		ContractEntity entity = ContractEntity.builder() //
+				.claimIds("1234").contractId("3456").id(UUID.randomUUID().toString()).build();
+		Map<String, String> param = new HashMap<>();
+		param.put("state", "CA");
+		param.put("numofdays", "5");
+		param.put("receiveddate", LocalDate.now().toString());
+		
+				
+		when(tibcoClient.fetchContracts(param)).thenReturn(Lists.list(entity));
+		LocalDate cutOffDays  = LocalDate.now().minusDays(5);
+		IngestionControllerEntity controllEntity = IngestionControllerEntity.builder()
+				.states("CA")
+				.cutOffDate(Date.valueOf( cutOffDays))
+				.noOfDays(5)
+				.build();
+		contractIngester.ingest(controllEntity);
+		Mockito.verify(tibcoClient, times(1)).fetchContracts(ArgumentMatchers.any(Map.class));
+		
+	}
+	
+
+}
